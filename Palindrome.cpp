22c7a0db43ea177aee3a46e748d27df78d13087c
U-BSD\455644 <455644@SHS-10D9C06CC.beaverton.k12.or.us>
2021-09-24 11:43:52 -0700

    Some changes almost done

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,32 +3,33 @@
 using namespace std;
 
 int main(){
-  char str[81];
-  char str2[81];
-  char rstr[81];
+  char str[80];
+  char str2[80];
+  char rstr[80];
   cin.get(str, 80);
   int count = 0;
-  int numOfChars = 0;
+  int palindromeNum = 0;
   for(int i = 0; i < 80 && str[i]  !='\0'; i++){
     if(isalnum(str[i])){
       str2[count] = str[i];
       count++;
-      numOfChars++;
     }
   }
   for(int i = 0; i < count; i++){
     str2[i] = tolower(str2[i]);
   }
-  for(int i=0;i<sizeof(str)/2;i++){
-     char temp=str[i];
-     str[i]=str[sizeof(str)-i-1];
-     str[sizeof(str)-i-1]=temp;
-    }
   for(int i = 0; i < count; i++){
-    
+    rstr[i] = str2[count-i-1];
   }
-  cout << count << endl;
-  cout << str << endl;
-  cout << str2 << endl;
-  cout << rstr << endl;
+  for(int i = 0; i < count; i++){
+    if(rstr[i] == str2[i]){
+      palindromeNum++;
+      }
+  }
+  if(palindromeNum == count){
+    cout << "Palindrome" << endl;
+  }
+  else{
+    cout << "Not a Palindrome" << endl;
+  }
 }
